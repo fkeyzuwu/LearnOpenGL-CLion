443857f76cd2c23a3d62743f3caf53c4d99b4458
fkeyz <fkeyzuwu@gmail.com>
2024-12-30 02:31:14 +0200

    add assimp

---
+++ src/model.cpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "Model.h"
+
+void Model::Draw(Shader &shader)
+{
+    for(unsigned int i = 0; i < meshes.size(); i++)
+        meshes[i].Draw(shader);
+}
+
+void Model::loadModel(std::string path)
+{
+    Assimp::Importer import;
+    const aiScene *scene = import.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs);
+
+    if(!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode)
+    {
+        std::cout << "ERROR::ASSIMP::" << import.GetErrorString() << std::endl;
+        return;
+    }
+    directory = path.substr(0, path.find_last_of('/'));
+
+    processNode(scene->mRootNode, scene);
+}
+
