7c22a82f5e5b8a54c85049fa8fb969d0c63f3fd9
fkeyz <fkeyzuwu@gmail.com>
2024-12-30 03:29:51 +0200

    added model

--- src/model.h
+++ src/model.h
@@ -8,19 +8,24 @@
 class Model
 {
 public:
-    Model(char *path)
+    Model(std::string const &path, bool gamma = false) : gammaCorrection(gamma)
     {
         loadModel(path);
     }
+
     void Draw(Shader &shader);
-private:
+    std::vector<Texture> textures_loaded;
     // model data
     std::vector<Mesh> meshes;
     std::string directory;
-
+    bool gammaCorrection;
+private:
     void loadModel(std::string path);
     void processNode(aiNode *node, const aiScene *scene);
     Mesh processMesh(aiMesh *mesh, const aiScene *scene);
     std::vector<Texture> loadMaterialTextures(aiMaterial *mat, aiTextureType type,
-                                         std::string typeName);
+                                              std::string typeName);
 };
+
+
+unsigned int TextureFromFile(const char* path, const std::string& directory, bool gamma = false);
